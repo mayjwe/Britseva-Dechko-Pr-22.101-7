beb9f78d4cc526944a884d64958ec3ee213e33a3
yana <anaa37906@gmail.com>
2024-05-28 10:43:53 +0700

    Britseva, Dechko; Pr-22.101; 7 EDIT name 24

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "NastyaVera",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
