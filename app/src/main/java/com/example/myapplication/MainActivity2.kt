62cdeba03db35b8e945233b889bef559a05bfd63
yana <anaa37906@gmail.com>
2024-05-28 10:30:14 +0700

    Britseva, Dechko; Pr-22.101; 7 11122

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Scaffold
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import java.lang.reflect.Modifier
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+                }
+            }
+        }
